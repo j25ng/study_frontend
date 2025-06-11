7f313572a3d5cd9798d896a5cb91eb21d12b9ad4
j25ng <jsan2861@gmail.com>
2025-06-11 14:20:03 +0900

    Study JavaScript

---
+++ js/array.js
@@ -0,0 +1,84 @@
+// const array = [1, 2, 3, 4, 5]
+
+// const humanArray = ['홍길동', '김철수', '김지민']
+
+// const objectArray = [
+//     { name: '홍길동' },
+//     { name: '김철수' },
+//     { name: '김지민' },
+// ];
+
+// for (let i = 0; i < humanArray.length; i++) {
+//     console.log(humanArray[i]);
+// }
+
+// array.push(6);  // (what to add)
+// console.log(array);
+
+// array.pop();    // pop은 마지막 요소를 삭제합니다.
+// console.log(array);
+
+// array.unshift(0);   // (what to add)
+// console.log(array);
+
+// array.shift();  // (how many to delete)
+// console.log(array);
+
+// array.splice(2, 0, 3); // (index, how many to delete, what to add)
+// console.log(array);
+
+// array.splice(2, 1); // (index, how many to delete)
+// console.log(array);
+
+// const copyArray = array.slice(1, 3); // (start index, end index - not included)
+// console.log(copyArray);
+
+// console.log(array.concat(copyArray));
+// // array.concat(...copyArray); // (what to add)
+
+// const mapArray = array.map((e) => e * 2); // map은 배열의 각 요소를 변환하여 새로운 배열을 만듭니다.
+// console.log(mapArray);
+
+// const filterArray = array.filter((e) => e > 2); // filter는 조건에 맞는 요소만을 포함하는 새로운 배열을 만듭니다.
+// console.log(filterArray);
+
+// const findArray = array.find((e) => e === 3); // find는 조건에 맞는 첫 번째 요소를 반환합니다.
+// console.log(findArray);
+
+// reduce(acc, cur) : acc = 누적값, cur = 현재값
+// const reduceArray = array.reduce((acc, cur) => acc + cur, 0); // reduce는 배열의 모든 요소를 누적하여 하나의 값을 만듭니다.
+// console.log(reduceArray);
+
+// const nonSortArray = [100, 20, 4, 3000];
+// const sortArray = nonSortArray.sort();
+// const realSortArray = nonSortArray.sort((a, b) => a - b); // sort는 배열을 정렬합니다. 기본적으로 문자열로 정렬되므로 숫자로 정렬하려면 비교 함수를 사용해야 합니다.
+// console.log(realSortArray);
+
+const array1 = [];
+const array2 = [];
+
+for (let i = 0; i <= 100; i++) {
+    array1.push(i);
+}
+
+for (let i = 101; i <= 200; i++) {
+    array2.push(i);
+}
+
+console.log(array1);
+console.log(array2);
+
+const evenArray = array1.concat(array2).filter((e) => e % 2 === 0); // 짝수만 필터링
+console.log(evenArray);
+
+console.log(evenArray.sort((a, b) => b - a)); // 내림차순 정렬
+
+console.log(evenArray.reduce((acc, cur) => acc + cur, 0)); // 모든 요소의 합계
+
+const oneComboArray = array1
+    .concat(array2)
+    .filter((e) => e % 2 === 0)
+    .sort((a, b) => b - a)
+    .reduce((acc, cur) => acc + cur, 0);
+
+console.log(oneComboArray); // 짝수만 필터링, 내림차순 정렬, 합계
--- js/loop.js
+++ js/loop.js
@@ -1,18 +1,77 @@
 // for 반복문
-for (let i = 0; i < 10; i++) {
-    console.log('지금 현재 i 값은', i, '입니다.');
-}
+// for (let i = 0; i < 10; i++) {
+//     console.log('지금 현재 i 값은', i, '입니다.');
+// }
 
-// while 반복문
-let j = 0;
-while (j < 10) {
-    console.log('지금 현재 j 값은', j, '입니다.');
-    j++;
-}
+// // while 반복문
+// let j = 0;
+// while (j < 10) {
+//     console.log('지금 현재 j 값은', j, '입니다.');
+//     j++;
+// }
 
-// do while 반복문
-let k = 0;
-do {
-    console.log('지금 현재 k 값은', k, '입니다.');
-    k++;
-} while (k < 10);
+// // do while 반복문
+// let k = 0;
+// do {
+//     console.log('지금 현재 k 값은', k, '입니다.');
+//     k++;
+// } while (k < 10);
+
+
+// const person = {
+//     name: '황지민',
+//     age: 29,
+//     job: '개발자',
+// };
+
+// for (key in person) {
+//     console.log('key: ', key);
+
+//     if (key === 'job') {
+//         if (person[key] === '개발자') {
+//             console.log('개발자입니다.');
+//         } else {
+//             console.log('30세 이상입니다.');
+//         }
+//     }
+//     // console.log(person[key]);
+// }
+
+const persons = {
+    황지민: {
+        age: 29,
+        job: '개발자',
+        sex: '남',
+    },
+    홍길동: {
+        age: 30,
+        job: '개발자',
+        sex: '남',
+    },
+    홍지민: {
+        age: 25,
+        job: '디자이너',
+        sex: '여',
+    },
+    홍동길: {
+        age: 35,
+        job: '개발자',
+        sex: '남',
+    }
+};
+
+for (i in persons) {
+    if (persons[i].age < 30 && persons[i].sex === '여') {
+        console.log(i, '은(는) 30세 미만의 여성입니다.');
+    }
+};
+
+for (i in persons) {
+    for (j in persons[i]) {
+        if (j === 'age' && persons[i][j] < 30) {
+            if (persons[i].sex === '여') {
+                console.log(i, '은(는) 30세 미만의 여성입니다.');
+            }
+        }
+    }
+};
