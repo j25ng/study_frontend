e53d84144f03456ec42049c3e8220fc5a276be73
j25ng <jsan2861@gmail.com>
2025-07-03 19:19:22 +0900

    React Practice

---
+++ practice/react_deepening_routing/src/pages/Layout.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+// 1. Outlet을 사용하여 모든 라우팅에서 보이게 해야합니다.
+// 2. 각 Link를 연동하여 알맞는 주소로 전송 해야 합니다.
+
+function Layout() {
+  return (
+    <div className="admin-layout">
+      <header>
+        <div className="logo">어드민 대시보드</div>
+        <h2>Admin Page Project</h2>
+      </header>
+      <div className="admin-content">
+        <div className="sidebar">
+          <Link to=".">대시보드</Link>
+          <Link to="users">사용자</Link>
+          <Link to="settings">설정</Link>
+          <Link to="notfound">잘못된 페이지</Link>
+        </div>
+        <div className="main-content">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
