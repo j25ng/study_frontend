3f7b0794be864e259179031f977ec08a44e7158a
j25ng <jsan2861@gmail.com>
2025-07-17 09:37:01 +0900

    nestjs

---
+++ server/server/src/main.js
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('TODO LIST')
+    .setDescription('The TODO LIST API')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3000);
+}
+
+bootstrap();
